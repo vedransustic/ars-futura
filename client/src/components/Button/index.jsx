c1bd740a0a0fb9745a728d258e2044672598e7b8
Vedran Sustic <78386759+vedransustic@users.noreply.github.com>
2022-04-26 20:18:41 +0200

    login and register frontend remade with axios and functionality

--- client/src/components/Button/index.jsx
+++ client/src/components/Button/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./index.css";
 
-const NewListButton = ({ text }) => {
+const NewListButton = ({ text, disable }) => {
   return (
-    <button className="button">
+    <button className="button" disabled={disable}>
       <span className="button-text">{text}</span>
     </button>
   );
--- client/src/const/index.js
+++ client/src/const/index.js
@@ -1,3 +1,11 @@
+export const USERNAME_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+export const PASSWORD_REGEX =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+export const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+export const REGISTER_URL = "/register";
+export const LOGIN_URL = "/login";
+
 export const DUMMY_USER = {
   name: "Vedran",
   email: "sustic_vedran@yahoo.com",
